25d2f663323b755c06f082626498ac371d636c45
Pablo <oscar282004@gmail.com>
2022-12-02 02:39:40 +0100

    Test fonct° models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Walido(models.Model):
+    name = models.CharField(max_length=128)
+    number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
